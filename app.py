d4a14eaa7ad301c18db3eb17135efabc76385da0
tmbiro <tifanibiro@Tifanis-MacBook-Pro.local>
2023-03-08 17:19:59 -0500

    Added temp route

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import numpy as np
-import datetime as dt
+import pandas as pd
 
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -40,50 +38,25 @@ def welcome():
     return (
         f"<h3>Available Routes:</h3><br/>"
         f"<b>Weather stations:</b> <br/>/api/v1.0/stations<br/>"
-        f"<br/><b>Precipitation measurements:</b> <br/>/api/v1.0/precipitation<br/>"
-        f"<br/><b>Temperature measurements:</b> <br/>/api/v1.0/tobs<br/>"
+        f"<br/><b>Precipitation measurements for the last 12 months of data:</b> <br/>/api/v1.0/precipitation<br/>"
+        f"<br/><b>Temperature measurements for the last 12 months of the data:</b> <br/>/api/v1.0/tobs<br/>"
         f"<br/><b>Temperature information for a specific date <br/>(add date to end using YYYY-MM-DD format):</b> <br/>/api/v1.0/<start><br/>"
-        f"<br/><b>Temperature information for a date range <br/>(add start and end dates to end using YYYY-MM-DD format, with '/' in-between the dates):</b> <br/>/api/v1.0/<start>/<end>"
+        f"<br/><b>Temperature information for a date range <br/>(add start and end dates to end using YYYY-MM-DD format, with '/' in-between the dates):</b> <br/>/api/v1.0/<start>/<end><br/>"
+        f"<br/><b>Note: The last date recorded in the dataset is 2017-08-23"
     )
 
 @app.route("/api/v1.0/precipitation")
-def names():
+def precip():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of all precipitation measures from the latest year in the dataset"""
-    
-    # Find the most recent date in the dataset
-    recent_date = (
-    session #take the session we've opened
-    .query(Measurement) #Look in the Measurement dataset
-    .order_by(Measurement
-              .date #Order by date
-              .desc()) #In decending order
-    .first() #Pull the first row
-    .date) #Take the value in the "date" column
-    
-    # Design a query to retrieve the last 12 months of precipitation data and plot the results. 
-    last_date = ((
-        dt #Using datetime module
-        .datetime #retrieve datetime object
-
-    # Starting from the most recent data point in the database. 
-        .strptime(recent_date, '%Y-%m-%d') #grab the recent date as a string in '%Y-%m-%d'format
-
-    # Calculate the last date one year from the most recent date in data set.
-        - #subtract
-        dt
-        .timedelta(days=365)) #365 days from it
-        .strftime('%Y-%m-%d')) #put the result in '%Y-%m-%d' format
-
-    last_date
 
     # Perform a query to retrieve the data and precipitation scores
     last_year_data = (
         session
         .query(Measurement.date, Measurement.prcp) #query the measurement date and precipitation columns
-        .filter(Measurement.date >= last_date) #only keep dates that are greater than or equal to last_date
+        .filter(Measurement.date >= '2016-08-23') #only keep dates that are greater than or equal to '2016-08-23'
         .all()) #grab everything that matches the filter
 
     session.close()
@@ -100,6 +73,25 @@ def stations():
     session.close()
 
     return jsonify(dict(station_data))
+
+@app.route("/api/v1.0/tobs")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all temperature measures from the latest year in the dataset"""
+
+    # Perform a query to retrieve the data and precipitation scores
+    temp_data = (
+        session
+        .query(Measurement.date, Measurement.tobs) #query the measurement date and precipitation columns
+        .filter(Measurement.date >= '2016-08-23') #only keep dates that are greater than or equal to '2016-08-23'
+        .filter(Measurement.station == 'USC00519281') #only keep data from the most active station
+        .all()) #grab everything that matches the filter
+
+    session.close()
+
+    return jsonify(dict(temp_data))
 
 if __name__ == '__main__':
     app.run(debug=True)
